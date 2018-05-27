1c33ce99c6d8e3de6538df81b70f90eb199af682
Debsish Mahana <debasishmahana49@gmail.com>
2018-05-27 15:33:37 +0530

    Update 09-fill-missing-data.py

--- 10-cleaning-data-in-python/4-cleaning-data-for-analysis/09-fill-missing-data.py
+++ 10-cleaning-data-in-python/4-cleaning-data-for-analysis/09-fill-missing-data.py
@@ -13,7 +13,11 @@ INSTRUCTIONS
 -Use the .fillna() method to replace all the missing values in the Ozone column of airquality with the mean, oz_mean.
 -Hit 'Submit Answer' to see the result of filling in the missing values!
 '''
-import pandas as pd
+# Calculate the mean of the Ozone column: oz_mean
+oz_mean = airquality.Ozone.mean()
 
-airquality = pd.read_csv('../_datasets/airquality.csv')
+# Replace all the missing values in the Ozone column with the mean
+airquality['Ozone'] = airquality.Ozone.fillna(oz_mean)
 
+# Print the info of airquality
+print(airquality.info())
